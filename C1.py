fa122f13ee3bfa4d68045d91fbde53509c96650f
CreatorOfSystem <159632067+CreatorOfSystem@users.noreply.github.com>
2025-10-15 15:32:24 +0000

    modified:   C1.py

--- C1.py
+++ C1.py
@@ -1,32 +1,29 @@
 h = int(input())
 for _ in range(h):
-    n, m = map(int, input().split())
-    ch = list(map(int, input().split()))
+    ch, mn = map(int, input().split())
+    n = list(map(int, input().split()))
     m = int(input())
     # for 0 index
-    if n == 1: print("Yes")
+    if ch == 1: print("Yes")
     else:
         new = m-n[0]
-        amin = min(new, n[0])
-        amax = max(new, n[0])
-        if amin<=n[i+1] or amin<=m-n[1]:
-            n[0]==amin
-            for i in range(1,n-1):
-                new = m - n[i]
-                amin = min(new, n[i])
-                amax = max(new, n[i])
-                if n[i-1]<=amin and (amin<=n[i+1] or amin<=m-n[i+1]):
-                    n[i] = amin
-                elif n[i-1]<=amax and (amax<=n[i+1] or amax<=m-n[i+1]):
-                    n[i] = amax
-                else:
-                    print("NO")
-                    break
-            else:
-                print("Yes")
 
+        n[0] = min(new, n[0])
+
+        for i in range(1,ch):
+            new = m - n[i]
+            amin = min(new, n[i])
+            amax = max(new, n[i])
+            if n[i-1]<=amin:
+                n[i] = amin
+            elif n[i-1]<=amax:
+                n[i] = amax
+            else:
+                print("NO")
+                break
         else:
-            print("NO")
+            print("YES")
+
         
 
 
