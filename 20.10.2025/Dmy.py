419f7d9db346475de319cfd9f60d44259f6d2633
CreatorOfSystem <159632067+CreatorOfSystem@users.noreply.github.com>
2025-10-21 13:51:20 +0000

    1

--- 20.10.2025/Dmy.py
+++ 20.10.2025/Dmy.py
@@ -1,12 +1,10 @@
 t = int(input())
 for _ in range(t):
-    data = []
     y0 = set()
     y1 = set()
     n = int(input())
     for _ in range(n):
         a = tuple(map(int, input().split()))
-        data.append(a)
         if a[1] == 1:
             y1.add(a[0])
         else:
@@ -15,13 +13,15 @@ for _ in range(t):
     c = (n-2)*len(m)
     y0x = list(y0)
     y1x = list(y1)
-    for x in range(len(y0)-1):
-        for x2 in range(x+1, len(y0)):
-            if x2-x == 2 and (x+1 in y1):
+    ly0 = len(y0)
+    ly1 = len(y1)
+    for x in range(ly0-1):
+        for i in (1, 2):
+            if x+i < ly0   and  y0x[x+i]-y0x[x] == 2 and (y0x[x]+1 in y1):
                 c+=1
-    for x in range(len(y1)-1):
-        for x2 in range(x+1, len(y1)):
-            if x2-x == 2 and (x+1 in y0):
+    for x in range(ly1-1):
+        for i in (1, 2):
+            if x+i < ly1   and  y1x[x+i]-y1x[x] == 2 and (y1x[x]+1 in y0):
                 c+=1
     print(c)
 
